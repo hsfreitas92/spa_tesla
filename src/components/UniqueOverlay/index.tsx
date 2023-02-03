49b62d6072b428ce0d770272c1cc3f1a42f63a33
Henrique Silva de Freitas <69264667+hsfreitas92@users.noreply.github.com>
2023-02-03 09:14:33 -0300

    first commit

---
+++ src/components/UniqueOverlay/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useTransform } from 'framer-motion'
+
+import { useWrapperScroll } from '../Model'
+
+import { Container, Header, Logo, Burger, Footer } from './styles'
+
+const UniqueOverlay: React.FC = () => {
+  const { scrollYProgress } = useWrapperScroll()
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1])
+
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <Burger />
+      </Header>
+
+      <Footer style={{ opacity }}>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Footer>
+    </Container>
+  )
+}
+
+export default UniqueOverlay
